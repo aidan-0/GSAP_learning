3d2ade4c035c86c5e61a0cb7a7c88cdbe2a26dff
Aidan <am.mcdonald00@gmail.com>
2024-03-18 20:17:53 +0800

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+    <>
+      <h1 className='text-4xl font-bold text-center mt-10'>
+        Hello World
+      </h1>
+    
+    </>
+  )
+}
+
+export default App
